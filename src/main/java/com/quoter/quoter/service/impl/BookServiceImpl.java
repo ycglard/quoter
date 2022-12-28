5f6bb9935dbabde43416c6190d14a10e2947ff83
ycaglard <yildirimcaglardasdemir@gmail.com>
2022-12-29 01:18:38 +0300

    quoter/gutendex random id and plain html body of the book according to id have been fetched from project gutenberg.

--- src/main/java/com/quoter/quoter/service/impl/BookServiceImpl.java
+++ src/main/java/com/quoter/quoter/service/impl/BookServiceImpl.java
@@ -5,6 +5,7 @@ import com.quoter.quoter.model.Book;
 import com.quoter.quoter.repository.BookRepository;
 import com.quoter.quoter.service.BookService;
 import com.quoter.quoter.service.WebScrapeService;
+import org.jsoup.Jsoup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Random;
@@ -20,7 +22,9 @@ import java.util.Random;
 @Service
 public class BookServiceImpl implements BookService {
 
-    static String gutenbergEndpoint = "https://gutendex.com/";
+    static String gutenbergEndpoint = "https://gutendex.com/books";//move it to the config file
+
+    static String bookPlainTextFormat = "https://www.gutenberg.org/cache/epub/";
 
     @Autowired
     BookRepository bookRepository;
@@ -31,17 +35,27 @@ public class BookServiceImpl implements BookService {
     Logger logger = LoggerFactory.getLogger(BookServiceImpl.class);
 
     @Override
-    public String randomBook() {
+    public String randomBook() throws IOException {//get the id, fetch the text, parse it according to the given indexes
         RestTemplate restTemplate = new RestTemplate();
         Random r = new Random();
-        String gRequest = gutenbergEndpoint + "books";
-        String gResponse = restTemplate.getForEntity(gRequest,String.class).getBody();
+        StringBuilder plainTextUrl = new StringBuilder();
+        String gResponse = restTemplate.getForEntity(gutenbergEndpoint,String.class).getBody();
         int count = (int) getBookCount(gResponse);
+        int randomGeneratedValue = r.nextInt(count);
 
-        String gRandomResponse = restTemplate.getForEntity(gutenbergEndpoint + "books?ids=" + r.nextInt(count),String.class).getBody();
-        return gRandomResponse;
+        plainTextUrl.append(bookPlainTextFormat)
+                .append(randomGeneratedValue)
+                .append("/pg")
+                .append(randomGeneratedValue)
+                .append(".txt");
+
+        return Jsoup.connect(plainTextUrl.toString()).get().html();
+
     }
-
+    //https://www.gutenberg.org/cache/epub/69640/pg69640-images.html
+    //https://www.gutenberg.org/cache/epub/69638/pg69638-images.html
+    //https://www.gutenberg.org/cache/epub/69638/pg69638.txt -> plain text
+    //https://www.gutenberg.org/cache/epub/2643/pg2643.txt -> id'yi artırarak ilerleyince sonuç veriyor
     private Object getBookCount(String json){
         ObjectMapper mapper = new ObjectMapper();
         try{
